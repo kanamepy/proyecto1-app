436766e6d86e3a3c368f136e84547a848a46e027
Vanesa Gomez <vanesa.saldivar89@gmail.com>
2023-11-18 20:43:49 -0300

    Delete resources directory

--- resources/views/about.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layout')
-@section('content')
-    
-        <h1>About</h1>
-    
-@endsection
--- resources/views/home.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout')
-@section('title','Home')
-
-@section('content')
-    <h1> Home cuerpo</h1>
-
-@endsection
